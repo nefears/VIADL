690329704da6fa04c9fb8ca966cd48e237e55e8a
nefears <nefears90@gmail.com>
2021-05-17 12:36:58 -0400

    Added loop through each participant folder to output a combined, analyzable data file in CSV

--- read_filenames.py
+++ read_filenames.py
@@ -1,6 +1,9 @@
 def read_filenames(filestofind):
-    import os
+    import os, re
     ##returns list of filenames to with
+    compiledexp = str()
+    for substring in filestofind:
+        compiledexp += '(?=.*'+substring+')'
     for root, dirs, files in os.walk("."):
-        files = [filename for filename in files if filename.endswith((filestofind))]
+        files = [filename for filename in files if bool(re.search(compiledexp, filename))]
         return files
