68a1f1b2b42ed7eb2fba36792b01ac3aaa7e38bc
Nicholas Fears <nefears90@gmail.com>
2021-05-27 14:11:48 -0400

    fixed jerk cost calculation

--- jerkcost.py
+++ jerkcost.py
@@ -1,11 +1,11 @@
-def jerkcost(x, frametime, framerate, max_vel, y=None, z=None):
+def jerkcost(xjerktraj, MovementDur, framerate, max_vel, yjerktraj=None, zjerktraj=None):
     #calculate jerk cost
     import numpy
-    if y and z:
+    if yacctraj and zacctraj:
        # print('3D Jerk')
-       sumval = numpy.square(x) + numpy.square(y) + numpy.square(z)
+       sumval = xjerktraj**2 + yjerktraj**2 + zjerktraj**2
     else:
        # print('2D Jerk')
-       sumval = numpy.square(x)
-    jerkcost_out = -numpy.log(abs(numpy.trapz(sumval)*1/framerate*frametime**3/max_vel**2))
+       sumval = xjerktraj**2
+    jerkcost_out = -numpy.log(abs(numpy.trapz(sumval)*1/framerate*MovementDur**3/max_vel**2))
     return jerkcost_out
